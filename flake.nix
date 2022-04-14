4be6538b203380cd347cf7a1c60cbaf4ffd16f8d
case <case@capsulecorp.org>
2022-04-14 10:55:07 -0400

    init, wip

---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  description = "sslscan";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    utils.url = "github:numtide/flake-utils";
+    sslscan-src = {
+      url = "github:rbsec/sslscan";
+      flake = false;
+    };
+  };
+
+
+  outputs = { self, nixpkgs, utils, sslscan-src }:
+    let
+      systems = [ "x86_64-linux" "i686-linux" "aarch64-linux" "aarch64-darwin" "x86-64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+      nixpkgsFor = forAllSystems (system:
+        import nixpkgs {
+          inherit system;
+
+          overlays = [
+            self.overlay
+            (final: prev: {
+              openssl = prev.openssl.override {
+                configureScript = prev.configureScript // { x86_64-linux = "./Configure linux-x86_64 zlib";};
+                #configureFlags = prev.configureFlags ++ [ "zlib" ];
+                buildInputs = prev.buildInputs ++ [ final.pkgs.zlib ];
+              };
+            })
+          ];
+        }
+      );
+    in
+    {
+      overlay = final: prev: {
+        sslscan = with final; (stdenv.mkDerivation {
+          name = "sslscan";
+          src = sslscan-src;
+          nativeBuildInputs = [ gnumake gcc ];
+          buildInputs = [ openssl glibc ];
+
+          buildPhase = ''
+            make 
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp sslscan $out/bin
+          '';
+
+          meta = with lib; {
+            description = " sslscan tests SSL/TLS enabled services to discover supported cipher suites";
+            homepage = https://github.com/rbsec/sslscan;
+            license = licenses.gpl3;
+            maintainers = with maintainers; [ case ];
+          };
+        });
+      };
+
+      packages = forAllSystems (system: {
+        inherit (nixpkgsFor.${system}) sslscan;
+      });
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.sslscan);
+
+      devShell = forAllSystems (system:
+        with nixpkgsFor.${system}; pkgs.mkShell {
+          buildInputs = with pkgs; [
+            openssl
+            gcc
+            gnumake
+            glibc
+            zlib
+          ];
+        });
+    };
+}
